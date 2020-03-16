211960360be6eede918aa0d7441edfcbaffc4d0d
William Dodson <william.dodson@gmail.com>
2020-03-16 11:06:14 -0400

    Initial print styles completed

--- src/components/header.js
+++ src/components/header.js
@@ -2,19 +2,22 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
+const Header = ({ siteTitle, subTitle }) => (
   <header class="banner">
-    <h1 class="banner__title">{siteTitle}</h1>
-    <p class="banner__subtitle">Designer/Developer</p>
+    <img alt="" class="logo-icon" src={'./william-dodson-icon.svg'} role="presentation" />
+    <h1 class="banner__title" data-byline={subTitle}>{siteTitle}</h1>
+    <p class="banner__subtitle">{subTitle}</p>
   </header>
 )
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
+  subTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
   siteTitle: ``,
+  subTitle: ``,
 }
 
 export default Header
